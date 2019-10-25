9e2a9a2d92cb5ec9a255cc38490572141cc58772
mcw33 <mcw33@cs-ssh.cs.calvin.edu>
2019-10-25 17:04:57 -0400

    work

--- lab3/talk_client.py
+++ lab3/talk_client.py
@@ -14,14 +14,30 @@ parser.add_argument("-p", "--port", dest="port", type=int, default=12345,
 parser.add_argument("-v", "--verbose", action="store_true", dest="verbose",
                     help="turn verbose output on")
 args = parser.parse_args()
- 
-with socket.socket() as s:
-    s.connect((args.server, args.port))
-    username = args.name if args.name else socket.gethostname()
-    while True:
-        readable, writable, exceptional = select.select([s, sys.stdin], [], [s])
-        for r in readable:
-            if r == s:
-                print(r.recv(4096).decode("utf-8"))
-            else:
-                s.send((username + ": " + r.readline().rstrip()).encode())
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    try:
+        s.connect((args.server, args.port))
+        username = args.name if args.name else socket.gethostname()
+        s.settimeout(2)
+        while True:
+            readable, writable, exceptional = select.select([s, sys.stdin], [], [s, sys.stdin])
+            for r in readable:
+                if r == s:
+                    try:
+                        data = r.recv(2048).decode("utf-8")
+                        if not data:
+                            print("The server died")
+                            exit()
+                        print(data)
+                    except:
+                        print("Something went wrong")
+                        exit()
+                else:
+                    try:
+                        s.send((username + ": " + r.readline().rstrip()).encode())
+                    except:
+                        print("An error has occured")
+                        exit()
+    except:
+        print("Couldn't connect")
