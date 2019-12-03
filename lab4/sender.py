75d73fb1cdbf4bac5b1056a91424fef40b7ea0dc
Mark Wissink <markcwissink@gmail.com>
2019-12-03 00:51:55 -0500

    Send text files

--- lab4/sender.py
+++ lab4/sender.py
@@ -10,6 +10,7 @@ import sys
 
 parser = argparse.ArgumentParser(description="A prattle client")
 
+parser.add_argument("-f", "--filename", dest="filename", help="The file to send")
 parser.add_argument("-a", "--address", dest="address", default="127.0.0.1",
                     help="IP address (default: 127.0.0.1)")
 parser.add_argument("-p", "--port", dest="port", type=int, default=22222,
@@ -18,6 +19,11 @@ parser.add_argument("-v", "--verbose", action="store_true", dest="verbose",
                     help="turn verbose output on")
 args = parser.parse_args()
 
-data = "Hello, World!\n"
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.sendto(data.encode(), (args.address, args.port))
+print("Sending %s to %s:%s" % (args.filename, args.address, args.port))
+with open(args.filename, 'r') as f:
+    while True:
+        data = f.read(1024)
+        if data == '':
+            break
+        sock.sendto(data.encode(), (args.address, args.port))
